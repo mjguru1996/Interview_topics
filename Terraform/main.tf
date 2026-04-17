88131ffd8747e537601c6164566a8ccacc707470
Guruprasad Kannan <130130177+mjguru1996@users.noreply.github.com>
2026-04-17 21:56:13 +0530

    Update main.tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -3,3 +3,31 @@ resource "aws_instance" "Terraform-server" {
   instance_type = var.instance_type
   key_name = "Mumbai"
 }
+
+# Contional Variabes Example:
+
+# variable "environment" {
+#   description = "Environment for which the infrastructure is being provisioned"
+#   default = "dev"
+# }
+
+# variable "prod_subnet_cidr" {
+#   description = "CIDR block for production subnet"
+#   type        = string
+#   default     = "10.0.1.0/24"
+# }
+# variable "dev_subnet_cidr" {
+#   description = "CIDR block for development subnet"
+#   type        = string
+#   default     = "10.0.2.0/24"
+# }
+# variable "aws_security_group" "Jenkins" {
+#   description = "Security group for Jenkins server"
+#   name        = "Jenkins-SG"
+#   ingress {
+#     from_port   = 22
+#     to_port     = 22
+#     protocol    = "tcp"
+#     cidr_blocks = var.environment == "prod" ? [var.prod_subnet_cidr] : [var.dev_subnet_cidr]
+#   }
+# }
