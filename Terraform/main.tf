6699e336f21433969b8b49614204bea9cb64c988
Guruprasad Kannan <130130177+mjguru1996@users.noreply.github.com>
2026-04-17 19:55:24 +0530

    Updated Code

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "Terraform-server" {
-  ami = "ami-05d2d839d4f73aafb"
-  instance_type = "t3.micro"
+  ami = var.ami
+  instance_type = var.instance_type
   key_name = "Mumbai"
 }
---
+++ Terraform/variables.tf
@@ -0,0 +1,8 @@
+variable "instance_type" {
+  description = "Instance type used"
+  default = "t3.micro"
+}
+variable "ami" {
+  description = "ami used"
+  default = "ami-05d2d839d4f73aafb"
+}
